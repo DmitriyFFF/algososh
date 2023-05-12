adb0cc69318206acf025f738817f4c47dcb1c3ab
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-05-12 10:19:06 +0400

    check the second computer connection

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Input, } from "../ui/input/input";
+import { Button } from "../ui/button/button"; 
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form action="">
+        <Input></Input>
+        <Button></Button>
+      </form>
     </SolutionLayout>
   );
 };
