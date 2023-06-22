f7b9eabc068b04734938db285838b5fcd3c5ee6e
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-06-22 20:33:23 +0400

    feat: create ListPage -> add and delete functions

--- src/components/list-page/list-class.ts
+++ src/components/list-page/list-class.ts
@@ -1,9 +1,10 @@
 export class Node<T> {
   value: T
   next: Node<T> | null
-  constructor(value: T, next?: Node<T> | null) {
+  constructor(value: T, next: Node<T> | null = null) {
     this.value = value;
-    this.next = (next === undefined ? null : next);
+    this.next = next;
+    // this.next = (next === undefined ? null : next);
   }
 }
 
@@ -30,21 +31,23 @@ export class LinkedList<T> implements ILinkedList<T> {
   }
 
   append(element: T) {
-    const node = new Node(element);
+    const newNode = new Node(element);
+
     if (!this.head || !this.tail) {
-      this.head = node;
-      this.tail = node;
+      this.head = newNode;
+      this.tail = newNode;
+
       return this;
     }
-    this.tail.next = node;
-    this.tail = node;
-    this.size++;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    // this.size++;//??
 
-    // return this;
+    return this;//??
   }
 
   prepend(element: T) {
-    const node = new Node(element);
+    const node = new Node(element, this.head);
     if (!this.tail) {
       this.tail = node;
       // this.head = node;
@@ -53,24 +56,92 @@ export class LinkedList<T> implements ILinkedList<T> {
     }
     // this.tail.next = node;
     // this.tail = node;
-    this.size++;
 
-    // return this;
+    // this.size++;//??
+
+    return this;//??
   }
 
   deleteHead() {
+    if (!this.head) return null;
+    const deletedHead = this.head.value;
 
+    // this.head = this.head.next;
+
+    if(this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.tail = null;
+    }
+    this.size--;//???
+    return deletedHead;
   }
 
   deleteTail() {
+    if (!this.tail) return null;
+    const deletedTail = this.tail.value;
 
+    // this.head = this.head.next;
+
+    if(this.tail.next) {
+      this.tail = this.tail.next;
+    } else {
+      this.head = null;
+    }
+    this.size--;//???
+    return deletedTail;
   }
 
-  addByIndex() {
+  addByIndex(element: T, index: number) {
+    if (index < 0 || index > this.size) return;
 
+    if (index === 0) {
+      this.head = new Node(element, this.head);
+      return;
+    }
+
+    const newNode = new Node(element);
+    // let prev;
+
+    let current = this.head;
+    let count = 0;
+
+    if (current !== null) {
+      while (count < index) {
+        newNode.next = current.next;
+        current.next = newNode;
+        count++;
+        // current = current.next;
+      }
+    }
+
+    newNode.next = current;//??
+    // prev.next = newNode;
+    this.size++;//??
   }
 
-  deleteByIndex() {
+  deleteByIndex(index: number) {
+    // if (!this.head) return null;??
+    if (index === 0) {
+      return this.deleteHead();
+    }
+
+    let deletedNode = null;
+    let current = this.head;
+    let count = 0;
+
+    if (current !== null && current.next !== null) {
+      while (count < index) {
+        if (current.next === this.tail) {
+          this.tail = current;
+        }
+        deletedNode = current.next;
+        // current = current?.next;
+        count++;
+        // current = current.next;
+      }
+    }
+    return deletedNode;
 
   }
 
