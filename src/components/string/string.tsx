04f9841c9bdda4f2d1799f49cc24c0578d902812
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-08-12 22:49:39 +0400

    feat: add tests -> string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -12,6 +12,7 @@ import { DELAY_IN_MS } from "../../constants/delays";
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [isDisabled,setIsDisabled] = useState(false);
   const [array, setArray] = useState<TItem[]>([]);
   const MAX_INPUT_LENGTH = 12;
 
@@ -20,6 +21,7 @@ export const StringComponent: React.FC = () => {
   };
 
   const reverseString = async (inputArray: TItem[]) => {
+    setIsDisabled(true);
     setIsLoading(true);
     setArray([...inputArray]);
 
@@ -50,6 +52,7 @@ export const StringComponent: React.FC = () => {
       end --;
     }
     setIsLoading(false);
+    setIsDisabled(false);
   }
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -63,21 +66,25 @@ export const StringComponent: React.FC = () => {
 
   return (
     <SolutionLayout title="Строка">
-      <form className={styles.form} onSubmit={onSubmit}>
+      <form className={styles.form}  data-testid="formContainer" onSubmit={onSubmit}>
         <Input
+          data-testid="inputValue"
           type="text"
           value={inputValue}
           isLimitText={true}
           maxLength={MAX_INPUT_LENGTH}
           onChange={handleChange}
+          disabled={isDisabled}
         />
         <Button
+          data-testid="submitButton"
           type="submit"
           text="Развернуть"
           isLoader={isLoading}
+          disabled={!inputValue}
         />
       </form>
-      <div className={styles.circleContainer}>
+      <div className={styles.circleContainer} data-testid="circles">
         {array.map((item, index) => (
           <Circle letter={item.value} key={index} state={item.color} />
         ))}
